b637f473022827c73b7a69cddc4dee3ab5db58a6
LaaLkA <n.afonchenkov@yandex.ru>
2024-11-25 15:02:01 +0300

    Completed

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.repositories;
 
 import com.example.demo.model.User;
-import org.hibernate.sql.ast.spi.SqlAliasBase;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -10,12 +9,24 @@ import java.util.List;
 
 @Repository
 public class UserRepository {
+
+    /**
+     * Объект подключения к БД
+     */
     private final JdbcTemplate jdbcTemplate;
 
+    /**
+     * Конструктор класса
+     * @param jdbcTemplate объект подключения к БД
+     */
     public UserRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    /**
+     * Метод получения всех пользователей из БД
+     * @return список всех пользователей, которые есть в БД.
+     */
     public List<User> findAll() {
         String sql = "SELECT * FROM userTable;";
 
@@ -30,23 +41,43 @@ public class UserRepository {
         return jdbcTemplate.query(sql, userRowMapper);
     }
 
+
+    /**
+     * Метод сохранения пользователя в БД
+     * @param user объект пользователя, который сохраняется в БД.
+     * @return объект, сохранённый в БД.
+     */
     public User save(User user) {
         String sql = "INSERT INTO userTable (firstName, lastName) VALUES (?, ?)";
         jdbcTemplate.update(sql, user.getFirstName(), user.getLastName());
         return user;
     }
 
-    public void delete(int id) {
+    /**
+     * Метод удаления пользователя из БД по id
+     * @param id идентификатор пользователя
+     */
+    public void deleteById(int id) {
         String sql = "DELETE FROM userTable WHERE id = ?";
         jdbcTemplate.update(sql, id);
     }
 
+    /**
+     * Метод обновления полей пользователя в БД
+     * @param user объект пользователя с измененными данными.
+     * @return изменённый объект
+     */
     public User update(User user) {
         String sql = "UPDATE userTable SET firstName = ?, lastName = ? WHERE id = ?";
         jdbcTemplate.update(sql, user.getFirstName(), user.getLastName(), user.getId());
         return user;
     }
 
+    /**
+     * Метод поиска пользователя по id
+     * @param id идентификатор пользователя
+     * @return найденный объект пользователя в БД
+     */
     public User findById(int id) {
         String sql = "SELECT * FROM userTable WHERE id = ?";
         RowMapper<User> userRowMapper = (r,i) -> {
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -8,28 +8,58 @@ import java.util.List;
 
 @Service
 public class UserService {
+    /**
+     * Объект репозитория для работы с БД
+     */
     private final UserRepository userRepository;
 
+    /**
+     * Конструктор класса.
+     * @param userRepository репозиторий пользователей
+     */
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Получение списка всех пользователей
+     * @return список пользователей
+     */
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
+    /**
+     * Сохранение пользователей
+     * @param user объект пользователя для сохранения
+     * @return сохранённый объект пользователя
+     */
     public User saveUser(User user) {
         return userRepository.save(user);
     }
 
+    /**
+     * Удаление пользователя по идентификатору
+     * @param id идентификатор для удаления
+     */
     public void deleteUser(int id) {
-        userRepository.delete(id);
+        userRepository.deleteById(id);
     }
 
+    /**
+     * Обновление пользователя
+     * @param user объект обновлённого пользователя
+     * @return объект, который был обновлён
+     */
     public User updateUser(User user) {
         return userRepository.update(user);
     }
 
+    /**
+     * Поиск по идентификатору
+     * @param id идентификатор пользователя, которого ищем
+     * @return найденный пользователь
+     */
     public User findById(int id) {
         return userRepository.findById(id);
     }
